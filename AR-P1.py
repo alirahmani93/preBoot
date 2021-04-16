dc3fadbe31ee81ac0ca830e9e0f3c5fd2e3f686a
Ali Rahmani <ali93rahmani@gmail.com>
2021-04-16 12:44:10 +0430

    for test

--- AR-P1.py
+++ AR-P1.py
@@ -1,3 +1,6 @@
+"""
+https://quera.ir/problemset/contest/3539/%D8%B3%D8%A4%D8%A7%D9%84-%D8%AA%DA%A9%D8%B1%D9%82%D9%85%DB%8C
+"""
 #target= int(input("Target is: "))
 target = '654' # 6+5+4 =15 ==> 1+5 = 6
 T=list(target)
---
+++ AR-P5.py
@@ -0,0 +1,21 @@
+n = int(input('n:'))
+k = int(input('k:'))
+
+res1=[]
+res2=[]
+
+for i in range(1,n+1):
+	if i % 2==0:
+		res1.append(i)
+	if not (i%2==0):
+		res2.append(i)
+
+res1 = res1[::-1]
+answer=res2+res1
+
+print(answer , answer[k])
+
+'''
+1 3 5 7 9 10 8 6 4 2
+0 1 2 3 4  5  6 7 8 9
+'''
