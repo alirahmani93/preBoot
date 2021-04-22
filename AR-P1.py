fd5320d1c62c2d6b38729f7234c4c59a5d2fa9b4
Ali Rahmani <ali93rahmani@gmail.com>
2021-04-22 21:46:06 +0430

    final-
    #last line

--- AR-P1.py
+++ AR-P1.py
@@ -2,6 +2,7 @@
 https://quera.ir/problemset/contest/3539/%D8%B3%D8%A4%D8%A7%D9%84-%D8%AA%DA%A9%D8%B1%D9%82%D9%85%DB%8C
 """
 #target = '654' # 6+5+4 =15 ==> 1+5 = 6
+#<<<<<<< Updated upstream
 target=input('target: ')
 addd=list(target)
 number=0
@@ -18,3 +19,17 @@ try:
             print(y)
        
 except: None
+#=======
+"""
+target= (input("Target is: "))
+T=list(target)
+print(T)
+while len(T)>1:
+    x=T
+    if len(list(str(x))) > 1:
+        for i in T:
+            x+=(i)
+    else:
+        print(x)
+#>>>>>>> Stashed changes""
+"""
