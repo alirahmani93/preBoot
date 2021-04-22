20196a4922bb4af552f6798163bc9088ea62274b
Ali Rahmani <ali93rahmani@gmail.com>
2021-04-23 03:14:30 +0430

    finish project

--- Week2/AR-P3.py
+++ Week2/AR-P3.py
@@ -8,3 +8,29 @@ input:
 output:
 {str : 1,dict:2,list:1,tuple:1,set:0,}
 """
+x= ( "gold", 1000, ("navid check", 3542),["neda watch",500],{"sanad khone" :"felan ja"}, {"name" :"navid", "pass" :"qwerty 1234"} )
+number,string,lists,tuples,sets,dicts=0,0,0,0,0,0
+r=dict()
+for i in x:
+    #print(i)
+    if type(i)== str :
+        string += 1
+        r.update(str=string)
+    elif type(i)== dict :
+        dicts+=1
+        r.update(dicts=dicts)
+    elif type(i)== list :
+        lists+=1
+        r.update(lists=lists)
+    elif type(i)== set :
+        sets+=1
+        r.update(sets=sets)
+    elif type(i)== tuple :
+        tuples+=1
+        r.update(tuples=tuples)
+    elif type(i)== int :
+        number+=1
+        r.update(number=number)
+
+print(string,dicts,lists,tuples,sets,number)
+print(r)
